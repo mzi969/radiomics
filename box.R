91b78c531d86332f63e5c627f29be1a315a5c5d6
li yong <76776154+mzi969@users.noreply.github.com>
2022-04-25 15:56:16 +0800

    Update box.R

--- box.R
+++ box.R
@@ -2,10 +2,19 @@ rm(list=ls())
 library(ggpubr)
 setwd("D:/Desktop/1")
 getwd() 
+box<-read.csv("primary_box.csv")
+group <- box$group
+radsocre <-box$radscore
+tiff(file = "primary_box.tiff", res = 600, width = 4800, height = 3600, compression = "lzw")
+ggboxplot(box, x = "group", y = "radsocre",
+          color = "group",add="jitter", palette = "jama"
+                  )+
+  stat_compare_means()+theme(text=element_text(size=16,  family="serif"))
+dev.off()
+###########2##########
 box<-read.csv("validation_box.csv")
 group <- box$group
 radsocre <-box$radscore
-###########2##########
 tiff(file = "validation_box.tiff", res = 600, width = 4800, height = 3600, compression = "lzw")
 ggboxplot(box, x = "group", y = "radsocre",
           color = "group",add="jitter", palette = "jama"
